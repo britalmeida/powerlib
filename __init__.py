7bb163492bd884fc3d63743186c62e20500b6fb0
Bastien Montagne <montagne29@wanadoo.fr>
2016-11-02 11:56:25 +0100

    Add basic example of group selector from .blend
    Added two ways, through enum or through collection.
    
    Enum way is simpler, but needs additional caching, and afaik is not
    serachable in the UI.
    
    Collection way is a bit more tedious, but stores its own data itself,
    and is searcheable with prop_search() layout function.
    
    Note that both are not linked together (setting enum version does not
    alter collection choice and vice-versa).

--- __init__.py
+++ __init__.py
@@ -70,6 +70,8 @@ class SaveState:
 runtime_vars["save_state"] = SaveState.AllSaved
 
 
+runtime_vars["components_enum_cache"] = {}
+
 enum_component_type = EnumProperty(
     items=(
         ('INSTANCE_GROUPS', "Instance Groups", "", 'EMPTY_DATA', 0),
@@ -81,12 +83,18 @@ enum_component_type = EnumProperty(
     description="Type of an asset component",
 )
 
+class ComponentItem(PropertyGroup):
+    name = StringProperty()
+
 
 class Component(PropertyGroup):
     def update_filepath_rel(self, context):
         """Updates the filepath property after we picked a new value for
         filepath_rel via a file browser.
         """
+        #~ self.group = None
+        self.groups.clear()
+
         if self.filepath_rel == '':
             return
         # TODO: ensure path is valid
@@ -99,10 +107,39 @@ class Component(PropertyGroup):
         print('Updating library link to {}'.format(fp_rel_to_lib))
         self.filepath = fp_rel_to_lib
 
+        cache_key = self.filepath
+        enum_cache = runtime_vars["components_enum_cache"].setdefault(cache_key, [])
+        if self.filepath_rel == '//' + os.path.basename(bpy.data.filepath):
+            enum_cache[:] = [(g.name, g.name, "") for g in bpy.data.groups if not g.library]
+            for g in bpy.data.groups:
+                if g.library:
+                    continue
+                self.groups.add().name = g.name
+        else:
+            with bpy.data.libraries.load(self.absolute_filepath) as (data_from, data_to):
+                enum_cache[:] = [(gname, gname, "") for gname in data_from.groups]
+                for gname in data_from.groups:
+                    self.groups.add().name = gname
+
+    def components_enumf(self, context):
+        if not context or not context.window_manager:
+            return []
+        cache_key = self.filepath
+        return runtime_vars["components_enum_cache"].setdefault(cache_key, [])
+
     id = StringProperty(
         name="Name",
         description="Name for this component, eg. the name of a group",
     )
+
+    name_name = EnumProperty(
+        items=components_enumf,
+        name="Name_name",
+        description="Name for this component, eg. the name of a group",
+    )
+
+    group = StringProperty()
+    groups = CollectionProperty(type=ComponentItem)
 
     filepath = StringProperty(
         name="File path",
@@ -603,11 +640,14 @@ class ASSET_UL_asset_components(UIList):
         col.enabled = is_edit_mode
         col.prop(item, "filepath_rel", text="", emboss=is_edit_mode)
         # TODO: Make "check if we are in the same file as item.filepath_rel" efficient
-        if item.filepath_rel == '//' + os.path.basename(bpy.data.filepath):
-            pass
-            # Show a nice selector, because we have access to the local groups
-        col.prop(item, "name", text="", emboss=is_edit_mode)
-        #layout.template_ID(context.scene.objects, "active")
+        #~ if item.filepath_rel == '//' + os.path.basename(bpy.data.filepath):
+            #~ layout.template_ID(item, "active")
+            #~ # Show a nice selector, because we have access to the local groups
+        #~ else:
+            #~ col.prop(item, "name", text="", emboss=is_edit_mode)
+        row = col.row()
+        row.prop_search(item, "group", item, "groups", text="")
+        row.prop(item, "name_name", text="", emboss=is_edit_mode)
 
 
 class ASSET_UL_collection_assets(UIList):
@@ -766,6 +806,7 @@ class ASSET_PT_powerlib(Panel):
 # Registry ####################################################################
 
 classes = (
+    ComponentItem,
     Component,
     ComponentsList,
     AssetItem,
